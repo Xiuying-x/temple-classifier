8881fdc414b2923b30805383e091f90bb1169be3
Xiuying-x <tarnoveskayaa@mail.ru>
2026-05-17 16:25:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.title("🏛️ Интеллектуальный классификатор к
 MODEL_PATH = "temple_classifier_best.pth"
 
 # ⚠️ ВСТАВЬ СЮДА СВОЮ ССЫЛКУ ИЗ ГУГЛ ДИСКА МЕЖДУ КАВЫЧКАМИ:
-GITHUB_RELEASE_UR = "https://github.com/Xiuying-x/temple-classifier/releases/download/v1.0.0/temple_classifier_best.pth"
+GITHUB_RELEASE_URL = "https://github.com/Xiuying-x/temple-classifier/releases/download/v1.0.0/temple_classifier_best.pth"
 
 @st.cache_resource
 torch.hub.download_url_to_file(GITHUB_RELEASE_URL, MODEL_PATH, progress=True)
